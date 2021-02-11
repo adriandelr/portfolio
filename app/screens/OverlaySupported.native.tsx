3a0389b88fd462a3f3cb4fd575e1e72cda14852f
adriandelr <adrian.delr@gmail.com>
2021-02-12 02:03:43 +0800

    Hide native view

--- app/screens/OverlaySupported.native.tsx
+++ app/screens/OverlaySupported.native.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function OverlaySupported() {
-  return <View></View>;
+  return <View style={{ display: "none" }}></View>;
 }
